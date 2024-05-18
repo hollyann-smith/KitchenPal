b83dcad9bb1d756b154bf059274d1cd3ba77fb41
Holly Smith <hollyschuldt@gmail.com>
2024-05-18 11:18:24 -0500

    got api calls working

---
+++ api/recipeData.js
@@ -0,0 +1,65 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getRecipes = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/ingredients.json"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// CREATE
+const createRecipe = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/recipe.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// GET SINGLE
+const getSingleRecipe = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/recipes/${id}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// DELETE
+const deleteSingleRecipe = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/recipes/${id}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getRecipes, createRecipe, getSingleRecipe, deleteSingleRecipe,
+};
--- components/NavBar.js
+++ components/NavBar.js
@@ -11,14 +11,20 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>KitchenPal</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+            <Link passHref href="/inventory">
+              <Nav.Link>Inventory</Nav.Link>
+            </Link>
+            <Link passHref href="/equipment">
+              <Nav.Link>Equipment</Nav.Link>
+            </Link>
+            <Link passHref href="/recipes">
+              <Nav.Link>Recipes</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
